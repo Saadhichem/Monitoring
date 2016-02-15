74c5f146473ab671327dc82b39af2310ec7922a3
saad hichem <saad.hichem@hotmail.com>
2016-02-15 18:32:13 +0100

    test

--- GithubTest/Form1.Designer.cs
+++ GithubTest/Form1.Designer.cs
@@ -44,7 +44,7 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(74, 126);
+            this.checkBox1.Location = new System.Drawing.Point(108, 129);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(80, 17);
             this.checkBox1.TabIndex = 1;
